b61ddc4491f8732afbe7fb87c78aa035c0ea3131
Thaynara19 <144859274+Thaynara19@users.noreply.github.com>
2024-02-06 17:40:39 -0300

    Create exe1.py

---
+++ exe1.py
@@ -0,0 +1,5 @@
+#isa pode ser usado para identificar se a informação que esta dando é verdadeira ou falsa em relação aos numero,nome e muito mais.
+nome = input(' Seu nome : ')
+print(nome.isalpha()) #Alfabetico
+idade = input('Sua idade : ')
+print(idade.isalnum())#Númerico
