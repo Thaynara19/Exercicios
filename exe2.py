420e4c112cd1f6d0ca0de05b52171d4cf8b9727d
Thaynara19 <144859274+Thaynara19@users.noreply.github.com>
2024-02-06 17:42:31 -0300

    Create exe2.py

---
+++ exe2.py
@@ -0,0 +1,5 @@
+#Exercicio Faça um progarama que ler um numero inteiro e mostre na tela seu sucessor e antecessor.
+num=int(input('Digite um número :'))
+num1 = num - 1
+num2 = num + 1
+print('O antecessor é : {} e o sucessor : {}'. format(num1,num2))
