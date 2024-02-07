6c782bee3d3b24a1ecbceec91d1055893418e165
Thaynara19 <144859274+Thaynara19@users.noreply.github.com>
2024-02-07 17:56:04 -0300

    Create exe4.py

---
+++ exe4.py
@@ -0,0 +1,5 @@
+# Exercicio que desenvolva um programa que leia duas notas e calcule a media.
+nota1 = float(input('Sua primeira nota : '))
+nota2 = float(input('Sua segunda nota : '))
+media = (nota1 + nota2)/2
+print('O resultado da sua notas é : ', media)
