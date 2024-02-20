2ef7f61003ca2506cd475bca622049dceaa316dc
Thaynara Alves <144859274+Thaynara19@users.noreply.github.com>
2024-02-19 22:35:05 -0300

    exe10.py

---
+++ exe10.py
@@ -0,0 +1,6 @@
+#Reajuste salarial/CURSO EM VIDEO
+
+salario = float(input('Informe seu salario R$: '))
+novo = salario + (salario * 15 / 100)
+print('Seu salario anterior era de R$: {} \nCom o novo reajuste ele passou a ser R$: {:.2f} '.format(salario,novo))
+#Criar um novo programa com o preço atual,avista e a prazo.
