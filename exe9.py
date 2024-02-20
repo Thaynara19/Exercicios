b0341867e0f45412b64d7bfc729672b105a45adc
Thaynara Alves <144859274+Thaynara19@users.noreply.github.com>
2024-02-19 22:33:57 -0300

    exe9.py

---
+++ exe9.py
@@ -0,0 +1,4 @@
+#Calculando desconto/CURSO EM VIDEO
+produto = float(input('Valor do produto :'))
+desc = produto - (produto * 5)/100
+print('O valor do produto : {} ,com desconto de 5% o novo valor dele é : {}'.format(produto,desc))
