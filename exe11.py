5a244a933aa9e787f1bae6cd67f639b60a947749
Thaynara Alves <144859274+Thaynara19@users.noreply.github.com>
2024-02-21 17:25:04 -0300

    Create exe11.py

---
+++ exe11.py
@@ -0,0 +1,5 @@
+#Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
+celsius = float(input('Informe a temperatura em ªC :'))
+fahrenheit = ((9*celsius) / 5) + 32
+print(f'A temperatura de {celsius} ªC corresponde a {fahrenheit}ªF ! ')
